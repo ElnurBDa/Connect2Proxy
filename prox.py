14ff6fbe25a88f239dde8188d931d0a087259414
Elnur Badalov <elnur.badalov.std@bhos.edu.az>
2024-09-01 20:55:18 +0400

    init

---
+++ prox.py
@@ -0,0 +1,17 @@
+import pexpect
+import time
+# Login
+child = pexpect.spawn('ssh elnur@192.168.209.130 -D 1080')
+child.expect("password:")
+child.sendline('elnur')
+# Ensure Interface appeared
+child.expect(r'elnur@elnur:~\$')
+child.sendline('ip -c=always a show gpd0')
+child.expect(r'elnur@elnur:~\$')
+print(child.before.decode())
+# End Connection
+input("Press Enter to Stop The Proxy")
+child.sendline('logout')
+child.expect(pexpect.EOF)
+
+
